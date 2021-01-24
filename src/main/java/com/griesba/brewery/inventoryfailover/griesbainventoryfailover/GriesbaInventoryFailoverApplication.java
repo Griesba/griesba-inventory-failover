cc0e82f0fad2dfd1589059b26274e280b3432789
griesba <griesba.pat@hotmail.com>
2021-01-24 19:56:54 +0100

    Project setup

---
+++ src/main/java/com/griesba/brewery/inventoryfailover/griesbainventoryfailover/GriesbaInventoryFailoverApplication.java
@@ -0,0 +1,13 @@
+package com.griesba.brewery.inventoryfailover.griesbainventoryfailover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GriesbaInventoryFailoverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GriesbaInventoryFailoverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/griesba/brewery/inventoryfailover/griesbainventoryfailover/GriesbaInventoryFailoverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.griesba.brewery.inventoryfailover.griesbainventoryfailover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GriesbaInventoryFailoverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
